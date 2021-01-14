407fab40f4f5b81b42abc77dda6671a50525d85c
dkrasnykh <krasnykh.daria@gmail.com>
2021-01-14 18:20:00 +0300

    fix consumer -> threaded consumer

--- src/main/java/com/orioninc/combplanapplicationservice/CombplanApplicationServiceApplication.java
+++ src/main/java/com/orioninc/combplanapplicationservice/CombplanApplicationServiceApplication.java
@@ -4,24 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @PropertySource({
-		"classpath:kafka.properties"
+        "classpath:kafka.properties"
 })
 public class CombplanApplicationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CombplanApplicationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CombplanApplicationServiceApplication.class, args);
+    }
 
-	@Bean
-	public ObjectMapper objectMapper() {
-		return new ObjectMapper();
-	}
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
 
 }
--- src/main/java/com/orioninc/combplanapplicationservice/consumer/RequestConsumerService.java
+++ src/main/java/com/orioninc/combplanapplicationservice/consumer/RequestConsumerService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.orioninc.combplanapplicationservice.dto.RequestDto;
 import com.orioninc.combplanapplicationservice.service.RequestService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -15,40 +16,70 @@ import org.springframework.stereotype.Service;
 import javax.annotation.PostConstruct;
 import java.time.Duration;
 import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service
 public class RequestConsumerService {
     private final ObjectMapper mapper;
     private final RequestService requestService;
-    private final KafkaConsumer<Long, String> consumer;
+    private final Properties properties;
+    private volatile boolean doneConsuming = false;
+    private ExecutorService executorService;
 
     @Value("${kafka.topic.request}")
     private String topic;
 
     @Autowired
-    public RequestConsumerService(ObjectMapper mapper, RequestService requestService, KafkaConsumer<Long, String> consumer) {
+    public RequestConsumerService(ObjectMapper mapper, RequestService requestService, Properties properties) {
         this.mapper = mapper;
         this.requestService = requestService;
-        this.consumer = consumer;
+        this.properties = properties;
     }
 
     @PostConstruct
     public void consume() {
-        consumer.subscribe(Collections.singletonList(topic));
-        try {
-            while (true) {
-                ConsumerRecords<Long, String> records = consumer.poll(Duration.ofMillis(1000));
-                for (ConsumerRecord<Long, String> record : records) {
-                    String value = record.value();
-                    log.info("=> consumed {}", value);
-                    RequestDto requestDto = readValue(value);
-                    requestService.createRequest(requestDto);
+        startConsuming();
+    }
+
+    public void startConsuming() {
+        executorService = Executors.newSingleThreadExecutor();
+        Runnable consumerThread = getConsumerThread(properties);
+        executorService.submit(consumerThread);
+    }
+
+    private Runnable getConsumerThread(Properties properties) {
+        return () -> {
+            Consumer<Long, String> consumer = null;
+            try {
+                consumer = new KafkaConsumer<>(properties);
+                consumer.subscribe(Collections.singletonList(topic));
+                while (!doneConsuming) {
+                    ConsumerRecords<Long, String> records = consumer.poll(Duration.ofMillis(5000));
+                    for (ConsumerRecord<Long, String> record : records) {
+                        String value = record.value();
+                        log.info("=> consumed {}", value);
+                        RequestDto requestDto = readValue(value);
+                        requestService.createRequest(requestDto);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (consumer != null) {
+                    consumer.close();
                 }
             }
-        } finally {
-            consumer.close();
-        }
+        };
+    }
+
+    public void stopConsuming() throws InterruptedException {
+        doneConsuming = true;
+        executorService.awaitTermination(10000, TimeUnit.MILLISECONDS);
+        executorService.shutdownNow();
     }
 
     private RequestDto readValue(String value) {
